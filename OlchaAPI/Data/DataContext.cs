e8fb7d505cca2b7982fcc9d4c96055ac6a3983c8
jakhongirs <j.sagdullaev31@gmail.com>
2022-11-01 20:13:56 +0500

    feat: connection string and migrations

---
+++ OlchaAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OlchaAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
