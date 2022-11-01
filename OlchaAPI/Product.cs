a890a667dea3f8612fde8ea5247ee4374a6e8426
jakhongirs <j.sagdullaev31@gmail.com>
2022-11-01 18:22:45 +0500

    Add project files.

---
+++ OlchaAPI/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace OlchaAPI
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
