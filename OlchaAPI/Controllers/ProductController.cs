4bee05695eb98af43f212d89c2bec27ddc880869
jakhongirs <j.sagdullaev31@gmail.com>
2022-11-01 19:00:43 +0500

    feat: Product Post Api

---
+++ OlchaAPI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OlchaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+            {
+                new Product {
+                    Id = 1,
+                    Name="Xiaomi Mi Band 6",
+                    Description="Fitnes braslet, 32 MB, 125 mAh, Bluetooth v 5.0",
+                    Price=353000,
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+            products.Add(product);
+            return Ok(products);
+        }
+    }
+}
+
