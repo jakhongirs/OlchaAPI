331d935973840616564c380f0fbe8a26c2130dcb
jakhongirs <j.sagdullaev31@gmail.com>
2022-11-04 19:26:15 +0500

    feat: aws db instance

--- OlchaAPI/Program.cs
+++ OlchaAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OlchaDB"));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
